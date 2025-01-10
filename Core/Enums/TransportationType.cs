c7a5ada156cb976291aa6d75d0104168134923bc
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-10 20:09:04 +0300

    add TransportationType.cs enum

---
+++ Core/Enums/TransportationType.cs
@@ -0,0 +1,18 @@
+namespace Core.Enums;
+
+public enum TransportationType
+{
+    Bus,
+    Train,
+    Plane,
+    Car,
+    Boat,
+    Ferry,
+    Walking,
+    Cycling,
+    Taxi,
+    Motorbike,
+    Subway,
+    Tram,
+    Helicopter
+}

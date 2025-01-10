2bc679381d517f36c86a017fd2b3c8dbb699884e
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-10 20:08:14 +0300

    add Stop.cs

---
+++ Core/Entities/Stop.cs
@@ -0,0 +1,26 @@
+using System.Text.Json.Serialization;
+using Core.Enums;
+
+namespace Core.Entities;
+
+public class Stop : BaseEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    
+    public string Longitude { get; set; }
+    public string Latitude { get; set; }
+    
+    public int DistanceToNext { get; set; }
+    public int ArrivalTimeToNext { get; set; }
+    public TransportationType TransportationTypeToNextStop { get; set; }
+    
+    public string ImageUrl { get; set; }
+    
+    
+    public Guid VoyageId { get; set; } // 
+    // nav
+    [JsonIgnore] // prevent circular serialization
+    public Voyage Voyage { get; set; }
+}
+    

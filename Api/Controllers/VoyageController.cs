b5b69cd71d9b44be6e222905749b46e34a61efc3
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-10 23:45:15 +0300

    Add VoyageController.cs

---
+++ Api/Controllers/VoyageController.cs
@@ -0,0 +1,20 @@
+using Core.Entities;
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+public class VoyageController : ControllerBase
+{
+    // [HttpPost]
+    // public Task<ActionResult<Voyage>> CreateVoyage(CreateVoyageModel request)
+    // {
+    //     var voyage = new Voyage()
+    //     {
+    //         
+    //     };
+    //
+    //     return Ok(voyage);
+    // }
+}

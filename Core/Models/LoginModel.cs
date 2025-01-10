61324a2ae4be3a5c11df9c2d3580aee99b157ee9
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-10 18:14:48 +0300

    add LoginModel.cs

---
+++ Core/Models/LoginModel.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models;
+
+public class LoginModel
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}

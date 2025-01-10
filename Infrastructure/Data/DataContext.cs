9715b9c5843dcabcdfb7c70aca92a932f11d9a55
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-10 20:07:55 +0300

    add Voyages and Stops table configurations

--- Infrastructure/Data/DataContext.cs
+++ Infrastructure/Data/DataContext.cs
@@ -7,9 +7,22 @@ namespace Infrastructure.Data;
 public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
     public DbSet<VoyagerUser> Users { get; set; }
+    public DbSet<Voyage> Voyages { get; set; }
+    public DbSet<Stop> Stops { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.UseIdentityColumns();
         
+        modelBuilder.Entity<Voyage>()
+            .HasOne<VoyagerUser>(v => v.User)
+            .WithMany()
+            .HasForeignKey(v => v.UserId);
+
+        modelBuilder.Entity<Stop>()
+            .HasOne<Voyage>(s => s.Voyage)
+            .WithMany(v => v.Stops)
+            .HasForeignKey(s => s.VoyageId);
+            
         base.OnModelCreating(modelBuilder);
     }
 }

66c2720c38cc255d484d40dc6bf70f8b308fe1b3
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-10 20:07:28 +0300

    add Currency.cs

---
+++ Core/Enums/Currency.cs
@@ -0,0 +1,32 @@
+namespace Core.Enums;
+
+public enum Currency
+{
+    USD, // US Dollar
+    EUR, // Euro
+    GBP, // British Pound
+    JPY, // Japanese Yen
+    AUD, // Australian Dollar
+    CAD, // Canadian Dollar
+    CHF, // Swiss Franc
+    CNY, // Chinese Yuan
+    SEK, // Swedish Krona
+    NZD, // New Zealand Dollar
+    MXN, // Mexican Peso
+    SGD, // Singapore Dollar
+    HKD, // Hong Kong Dollar
+    NOK, // Norwegian Krone
+    KRW, // South Korean Won
+    TRY, // Turkish Lira
+    INR, // Indian Rupee
+    BRL, // Brazilian Real
+    ZAR, // South African Rand
+    RUB, // Russian Ruble
+    SAR, // Saudi Riyal
+    AED, // UAE Dirham
+    PLN, // Polish Zloty
+    IDR, // Indonesian Rupiah
+    THB, // Thai Baht
+    MYR, // Malaysian Ringgit
+    PHP, // Philippine Peso
+}

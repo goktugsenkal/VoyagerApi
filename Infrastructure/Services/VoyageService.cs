4e261291972cc72f656705f4c379a6fdf8166060
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-12 22:02:42 +0300

    add VoyageService to act in between Controller and Repository

---
+++ Infrastructure/Services/VoyageService.cs
@@ -0,0 +1,58 @@
+using Core.Entities;
+using Core.Interfaces;
+using Core.Models;
+
+namespace Infrastructure.Services;
+
+public class VoyageService
+    (IVoyageRepository voyageRepository, IStopRepository stopRepository)
+    : IVoyageService
+{
+    /// <summary>
+    /// Ties together VoyageRepository and StopRepository methods for creating
+    /// 1. Creates a voyage and saves it to the Voyages table
+    /// 2. Gets its id
+    /// 3. Maps CreateStopModel->Stop and saves them with relation to the voyage.
+    /// </summary>
+    /// <param name="createVoyageModel">CreateVoyageModel</param>
+    public async Task AddVoyageAsync(CreateVoyageModel createVoyageModel)
+    {
+        // map request to Voyage entity
+        var voyage = new Voyage 
+            // this voyage will get an id in the next
+            // function, "await voyageRepository.AddAsync(voyage)"
+        {
+            Title = createVoyageModel.Title,
+            Description = createVoyageModel.Description,
+            LocationName = createVoyageModel.LocationName,
+            StartDate = createVoyageModel.StartDate,
+            EndDate = createVoyageModel.EndDate,
+            StopCount = createVoyageModel.StopCount,
+            Currency = createVoyageModel.Currency,
+            ExpectedPrice = createVoyageModel.ExpectedPrice,
+            ActualPrice = createVoyageModel.ActualPrice,
+            VoyagerUserId = createVoyageModel.VoyagerUserId
+        };
+
+        // save the Voyage to get its id
+        await voyageRepository.AddAsync(voyage);
+
+        // map stops !!!from CreateStopModel to Stop!!!
+        var stops = createVoyageModel.Stops.Select(stop => new Stop
+        {
+            Name = stop.Name,
+            Description = stop.Description,
+            Longitude = stop.Longitude,
+            Latitude = stop.Latitude,
+            DistanceToNext = stop.DistanceToNext,
+            ArrivalTimeToNext = stop.ArrivalTimeToNext,
+            TransportationTypeToNextStop = stop.TransportationTypeToNextStop,
+            ImageUrl = stop.ImageUrl,
+            CreatedAt = stop.CreatedAt,
+            VoyageId = voyage.Id // set fk, voyageId
+        }).ToList();
+        
+        // save stops with relation to the Voyage
+        await stopRepository.AddRangeAsync(stops);
+    }
+}

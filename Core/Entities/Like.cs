8cde67756cb3e03e665ba662db6071d4087ef66c
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-13 02:17:09 +0300

    add summary to Like.cs

--- Core/Entities/Like.cs
+++ Core/Entities/Like.cs
@@ -4,30 +4,47 @@ using Core.Enums;
 namespace Core.Entities;
 
 /// <summary>
-/// 
+/// Represents a like given by a user on a resource, such as a voyage or a comment.
+/// This entity supports polymorphic relationships to either a Voyage or a Comment,
+/// while maintaining the association with the user who liked the resource.
 /// </summary>
 public class Like : BaseEntity
 {
-    // foreign key VoyageId references Voyages table
+    /// <summary>
+    /// Foreign key referencing the Voyage entity. Nullable to support polymorphism.
+    /// </summary>
     public Guid? VoyageId { get; set; }
-    
-    // foreign key CommentId references Comments table
+
+    /// <summary>
+    /// Foreign key referencing the Comment entity. Nullable to support polymorphism.
+    /// </summary>
     public Guid? CommentId { get; set; }
-    
+
+    /// <summary>
+    /// Indicates the type of resource being liked (Voyage or Comment).
+    /// </summary>
     public LikeType LikeType { get; set; }
 
-    // foreign key UserId references Users table
+    /// <summary>
+    /// Foreign key referencing the VoyagerUser entity.
+    /// </summary>
     public Guid VoyagerUserId { get; set; }
-    
-    // navigation to Voyage
-    [JsonIgnore] 
-    public Voyage Voyage  { get; set; }
-    
-    // navigation to Comment
-    [JsonIgnore] 
+
+    /// <summary>
+    /// Navigation property to the associated Voyage entity.
+    /// </summary>
+    [JsonIgnore]
+    public Voyage Voyage { get; set; }
+
+    /// <summary>
+    /// Navigation property to the associated Comment entity.
+    /// </summary>
+    [JsonIgnore]
     public Comment Comment { get; set; }
 
-    // navigation to User
-    [JsonIgnore] 
-    public VoyagerUser VoyagerUser  { get; set; }
+    /// <summary>
+    /// Navigation property to the associated VoyagerUser entity.
+    /// </summary>
+    [JsonIgnore]
+    public VoyagerUser VoyagerUser { get; set; }
 }

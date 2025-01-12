a9f5ea769ffc2487f4aec834988e196174aafcb9
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-13 01:44:47 +0300

    configure Like&Comment relationships

--- Infrastructure/Data/DataContext.cs
+++ Infrastructure/Data/DataContext.cs
@@ -22,7 +22,32 @@ public class DataContext(DbContextOptions<DataContext> options) : DbContext(opti
             .HasOne<Voyage>(s => s.Voyage)
             .WithMany(v => v.Stops)
             .HasForeignKey(s => s.VoyageId);
-            
+
+        modelBuilder.Entity<Like>()
+            .HasOne<Voyage>(l => l.Voyage)
+            .WithMany(v => v.Likes)
+            .HasForeignKey(l => l.VoyageId);
+        
+        modelBuilder.Entity<Like>()
+            .HasOne<VoyagerUser>(l => l.VoyagerUser)
+            .WithMany()
+            .HasForeignKey(l => l.VoyagerUserId);
+        
+        // (VoyageId, VoyagerUserId) pair is unique because 1 person can like something once
+        modelBuilder.Entity<Like>()
+            .HasIndex(l => new { l.VoyageId, l.VoyagerUserId })
+            .IsUnique();
+        
+        modelBuilder.Entity<Comment>()
+            .HasOne<Voyage>(c => c.Voyage)
+            .WithMany(c => c.Comments)
+            .HasForeignKey(c => c.VoyageId);
+        
+        modelBuilder.Entity<Comment>()
+            .HasOne<VoyagerUser>(c => c.VoyagerUser)
+            .WithMany()
+            .HasForeignKey(c => c.VoyagerUserId);
+        
         base.OnModelCreating(modelBuilder);
     }
 }

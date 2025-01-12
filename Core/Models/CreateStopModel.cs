7dca015e1605f0dd6ea1036d550867fa735e1dc3
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-12 22:29:46 +0300

    add CreateStopModel.cs

---
+++ Core/Models/CreateStopModel.cs
@@ -0,0 +1,15 @@
+using Core.Enums;
+
+namespace Core.Models;
+
+public class CreateStopModel
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Longitude { get; set; }
+    public string Latitude { get; set; }
+    public int DistanceToNext { get; set; }
+    public int ArrivalTimeToNext { get; set; }
+    public TransportationType TransportationTypeToNextStop { get; set; }
+    public string ImageUrl { get; set; }
+}

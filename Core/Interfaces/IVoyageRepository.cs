d9cfc6ba411153a5fcae8ffc25ffeafc4b380067
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-12 22:25:43 +0300

    style

--- Core/Interfaces/IVoyageRepository.cs
+++ Core/Interfaces/IVoyageRepository.cs
@@ -5,8 +5,8 @@ namespace Core.Interfaces;
 public interface IVoyageRepository
 {
     public Task<ICollection<Voyage>> GetAllVoyages();
-    public Task<Voyage> GetVoyageById(Guid voyageId);
-    public Task CreateVoyageAsync(Voyage voyage);
+    public Task<Voyage?> GetVoyageById(Guid voyageId);
+    Task AddAsync(Voyage voyage);
     public Task UpdateVoyage(Voyage voyage);
     public Task DeleteVoyage(Voyage voyage);
 }

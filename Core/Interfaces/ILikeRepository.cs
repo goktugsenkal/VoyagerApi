4710d0bf20ee0fce4320c96525c895e7f04f67bc
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-13 04:03:39 +0300

    add LikeRepository & interface

--- Core/Interfaces/ILikeRepository.cs
+++ Core/Interfaces/ILikeRepository.cs
@@ -1,6 +1,47 @@
+using Core.Entities;
+
 namespace Core.Interfaces;
 
 public interface ILikeRepository
 {
-    
+    /// <summary>
+    /// Checks if a like exists for the specified resource (Voyage or Comment) and user.
+    /// </summary>
+    /// <param name="voyageId">The ID of the voyage, or null if targeting a comment.</param>
+    /// <param name="commentId">The ID of the comment, or null if targeting a voyage.</param>
+    /// <param name="userId">The ID of the user.</param>
+    /// <returns>True if the like exists; otherwise, false.</returns>
+    Task<bool> ExistsAsync(Guid? voyageId, Guid? commentId, Guid userId);
+
+    /// <summary>
+    /// Adds a like to the database.
+    /// </summary>
+    /// <param name="like">The like entity to add.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task AddAsync(Like like);
+
+    /// <summary>
+    /// Removes a like from the database.
+    /// </summary>
+    /// <param name="like">The like entity to remove.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task RemoveAsync(Like like);
+
+    public Task<Like?> GetLikeAsync(Guid? voyageId, Guid? commentId, Guid userId);
+
+    /// <summary>
+    /// Retrieves all likes for a specified voyage or comment.
+    /// </summary>
+    /// <param name="voyageId">The ID of the voyage, or null if targeting a comment.</param>
+    /// <param name="commentId">The ID of the comment, or null if targeting a voyage.</param>
+    /// <returns>A list of likes.</returns>
+    Task<List<Like>> GetLikesAsync(Guid? voyageId, Guid? commentId);
+
+    /// <summary>
+    /// Counts the number of likes for a specific voyage or comment.
+    /// </summary>
+    /// <param name="voyageId">The ID of the voyage, or null if targeting a comment.</param>
+    /// <param name="commentId">The ID of the comment, or null if targeting a voyage.</param>
+    /// <returns>The total count of likes.</returns>
+    Task<int> CountLikesAsync(Guid? voyageId, Guid? commentId);
 }
--- Infrastructure/Repositories/LikeRepository.cs
+++ Infrastructure/Repositories/LikeRepository.cs
@@ -1,6 +1,67 @@
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Infrastructure.Repositories;
 
-public class LikeRepository
+public class LikeRepository(DataContext context) : ILikeRepository
 {
+    public async Task<bool> ExistsAsync(Guid? voyageId, Guid? commentId, Guid userId)
+    {
+        // returns true if
+        // the provided user has liked the specified voyage (when voyageId is not null).
+        // OR
+        // the provided user has liked the specified comment (when commentId is not null).
+        // userId and commentId SHOULD NEVER BE PROVIDED TOGETHER!!!
+        return await context.Likes.AnyAsync(l =>
+            l.VoyagerUserId == userId && 
+            (voyageId.HasValue && l.VoyageId == voyageId 
+             || 
+             commentId.HasValue && l.CommentId == commentId)
+        );
+    }
     
+    public async Task AddAsync(Like like)
+    {
+        await context.Likes.AddAsync(like);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task RemoveAsync(Like like)
+    {
+        context.Likes.Remove(like);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task<Like?> GetLikeAsync(Guid? voyageId, Guid? commentId, Guid userId)
+    {
+        return await context.Likes.FirstOrDefaultAsync(l =>
+            l.VoyagerUserId == userId &&
+            ((voyageId.HasValue && l.VoyageId == voyageId) || 
+             (commentId.HasValue && l.CommentId == commentId))
+        );
+    }
+
+
+
+    public async Task<List<Like>> GetLikesAsync(Guid? voyageId, Guid? commentId)
+    {
+        return await context.Likes
+            .Where(l =>
+                (voyageId.HasValue && l.VoyageId == voyageId) ||
+                (commentId.HasValue && l.CommentId == commentId)
+            )
+            .ToListAsync();
+    }
+
+    public async Task<int> CountLikesAsync(Guid? voyageId, Guid? commentId)
+    {
+        return await context.Likes
+            .CountAsync(l =>
+                (voyageId.HasValue && l.VoyageId == voyageId) ||
+                (commentId.HasValue && l.CommentId == commentId)
+            );
+    }
+
 }

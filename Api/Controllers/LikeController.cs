bb019c22160194db06231e5dee60075c8e7ccac5
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-14 21:25:48 +0300

    update: write only 2 methods, add and remove. Methods work for both voyages and comments

--- Api/Controllers/LikeController.cs
+++ Api/Controllers/LikeController.cs
@@ -15,7 +15,7 @@ public class LikeController(ILikeService likeService) : ControllerBase
     /// <param name="voyageId"></param>
     /// <param name="commentId"></param>
     /// <returns></returns>
-    [HttpPost("add")]
+    [HttpPost]
     [Authorize]
     public async Task<IActionResult> AddLikeAsync(Guid? voyageId, Guid? commentId)
     {
@@ -31,11 +31,13 @@ public class LikeController(ILikeService likeService) : ControllerBase
         // parse User's ID into a Guid
         var voyagerUserId = Guid.Parse(voyagerUserIdClaim.Value);
         
+        // if neither query parameter is provided
         if (voyageId == null && commentId == null)
         {
             return BadRequest("Either voyageId or commentId must be provided.");
         }
 
+        // if both query parameters are provided
         if (voyageId != null && commentId != null)
         {
             return BadRequest("Provide either a voyageId or a commentId, not both.");
@@ -43,14 +45,17 @@ public class LikeController(ILikeService likeService) : ControllerBase
 
         try
         {
+            // if voyageId is provided, call likeService with voyageId and User's ID
             if (voyageId != null)
             {
                 await likeService.AddLikeToVoyageAsync(voyageId.Value, voyagerUserId);
             }
+            // if commentId is provided, call likeService with commentId and User's ID
             else if (commentId != null)
             {
                 await likeService.AddLikeToCommentAsync(commentId.Value, voyagerUserId);
             }
+            // a like will be created accordingly and added to the database
 
             return Ok("Like added successfully.");
         }
@@ -64,41 +69,62 @@ public class LikeController(ILikeService likeService) : ControllerBase
         }
     }
 
-
-
     /// <summary>
     /// unlike a voyage
     /// </summary>
     /// <param name="voyageId"></param>
-    /// <returns></returns>
-    [HttpDelete("voyage/{voyageId:guid}")]
-    public async Task<IActionResult> RemoveLikeFromVoyage(Guid voyageId)
-    {
-        // Logic to remove a like from the voyage.
-        return NoContent();
-    }
-
-    /// <summary>
-    /// like a comment
-    /// </summary>
     /// <param name="commentId"></param>
     /// <returns></returns>
-    [HttpPost("comments/{commentId:guid}")]
-    public async Task<IActionResult> AddLikeToComment(Guid commentId)
+    [HttpDelete]
+    public async Task<IActionResult> RemoveLikeAsync(Guid? voyageId, Guid? commentId)
     {
-        // Logic to add a like to the comment.
-        return Ok();
-    }
+        // check Authorization header, then try to find the User's ID in the claims 
+        var voyagerUserIdClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+        
+        // return 401 if User's ID can't be found
+        if (voyagerUserIdClaim == null)
+        {
+            return Unauthorized("User ID not found in the token.");
+        }
+        
+        // parse User's ID into a Guid
+        var voyagerUserId = Guid.Parse(voyagerUserIdClaim.Value);
+        
+        // if neither query parameter is provided
+        if (voyageId == null && commentId == null)
+        {
+            return BadRequest("Either voyageId or commentId must be provided.");
+        }
 
-    /// <summary>
-    /// unlike a comment
-    /// </summary>
-    /// <param name="commentId"></param>
-    /// <returns></returns>
-    [HttpDelete("comments/{commentId:guid}")]
-    public async Task<IActionResult> RemoveLikeFromComment(Guid commentId)
-    {
-        // Logic to remove a like from the comment.
-        return NoContent();
+        // if both query parameters are provided
+        if (voyageId != null && commentId != null)
+        {
+            return BadRequest("Provide either a voyageId or a commentId, not both.");
+        }
+
+        try
+        {
+            // if voyageId is provided, call likeService with voyageId and User's ID
+            if (voyageId != null)
+            {
+                await likeService.RemoveLikeFromVoyageAsync(voyageId.Value, voyagerUserId);
+            }
+            // if commentId is provided, call likeService with commentId and User's ID
+            else if (commentId != null)
+            {
+                await likeService.RemoveLikeFromCommentAsync(commentId.Value, voyagerUserId);
+            }
+            // a like will be removed accordingly and updated in the database
+
+            return Ok("Like removed successfully.");
+        }
+        catch (InvalidOperationException ex)
+        {
+            return Conflict(new { Message = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { Message = "An unexpected error occurred.", Details = ex.Message });
+        }
     }
 }

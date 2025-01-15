77e63f8d603302a52f57f859ed5e35e66be2f07e
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-15 15:51:12 +0300

    add semantic versioning :D

--- Api/Program.cs
+++ Api/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Voyager API", Version = "v0.0.5" });
 
     // Add security definition for Bearer authentication
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme

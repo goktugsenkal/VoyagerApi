3e7b08c7875e202caf60cbb47fbf746222450859
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-15 14:06:07 +0300

    implement endpoints: get all comments for a voyage, update a comment, delete a comment

--- Api/Controllers/CommentController.cs
+++ Api/Controllers/CommentController.cs
@@ -10,12 +10,37 @@ namespace Api.Controllers;
 [Route("comment")]
 public class CommentController(ICommentService commentService) : ControllerBase
 {
+    /// <summary>
+    /// Get comments for a given voyage.
+    /// </summary>
+    /// <param name="voyageId">The ID of the voyage to get comments for.</param>
+    /// <returns>A list of comments for the given voyage.</returns>
     [HttpGet]
-    public IActionResult GetComments(Guid voyageId)
+    [Authorize]
+    public async Task<IActionResult> GetComments(Guid voyageId)
     {
-        return Ok();
+        var voyagerUserIdClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+        
+        // return 401 if User's ID can't be found
+        if (voyagerUserIdClaim == null)
+        {
+            return Unauthorized("User ID not found in the token.");
+        }
+        
+        // parse User's ID into a Guid
+        var voyagerUserId = Guid.Parse(voyagerUserIdClaim.Value);
+        
+        var comments = await commentService.GetCommentsByVoyageIdAsync(voyageId);
+        
+        return Ok(comments);
     }
     
+    /// <summary>
+    /// Adds a comment to a specified voyage. This endpoint requires authorization and retrieves the user ID from the token claims.
+    /// </summary>
+    /// <param name="voyageId">The ID of the voyage to which the comment is being added.</param>
+    /// <param name="commentModel">The model containing the details of the comment to be added.</param>
+    /// <returns>Returns a success message if the comment is added successfully, otherwise returns an error message.</returns>
     [HttpPost]
     [Authorize]
     public async Task<IActionResult> AddComment(Guid voyageId, CreateCommentModel commentModel)
@@ -39,6 +64,81 @@ public class CommentController(ICommentService commentService) : ControllerBase
             await commentService.AddCommentAsync(voyageId, voyagerUserId, commentModel);
             
             return Ok("Comment created successfully.");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    
+    /// <summary>
+    /// Updates an existing comment for a specified voyage. This endpoint requires authorization
+    /// and retrieves the user ID from the token claims to ensure the user is authorized to perform the update.
+    /// </summary>
+    /// <param name="voyageId">The ID of the voyage associated with the comment to be updated.</param>
+    /// <param name="commentId">The ID of the comment to be updated.</param>
+    /// <param name="commentModel">The model containing the updated details of the comment.</param>
+    /// <returns>Returns a success message if the comment is updated successfully, otherwise returns an error message.</returns>
+    [HttpPut]
+    [Authorize]
+    public async Task<IActionResult> UpdateComment(Guid voyageId, Guid commentId, CreateCommentModel commentModel)
+    {
+        // check Authorization header, then try to find the User's ID in the claims 
+        var voyagerUserIdClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+        
+        // return 401 if User's ID can't be found
+        if (voyagerUserIdClaim == null)
+        {
+            return Unauthorized("User ID not found in the token.");
+        }
+        
+        // parse User's ID into a Guid
+        var voyagerUserId = Guid.Parse(voyagerUserIdClaim.Value);
+        
+        try
+        {
+            // call VoyageService to do the saving of the Voyage and Stops
+            // give down voyageUserId that we got from the token's claims
+            await commentService.UpdateCommentAsync(voyageId, commentId, voyagerUserId, commentModel);
+            
+            return Ok("Comment updated successfully.");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    
+    /// <summary>
+    /// Deletes a comment associated with a specified voyage. 
+    /// This endpoint requires authorization and retrieves the user ID from the token claims 
+    /// to ensure the user is authorized to perform the deletion.
+    /// </summary>
+    /// <param name="commentId">The ID of the comment to be deleted.</param>
+    /// <returns>Returns a success message if the comment is deleted successfully, otherwise returns an error message.</returns>
+    [HttpDelete]
+    [Authorize]
+    public async Task<IActionResult> DeleteComment(Guid commentId)
+    {
+        // check Authorization header, then try to find the User's ID in the claims 
+        var voyagerUserIdClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+        
+        // return 401 if User's ID can't be found
+        if (voyagerUserIdClaim == null)
+        {
+            return Unauthorized("User ID not found in the token.");
+        }
+        
+        // parse User's ID into a Guid
+        var voyagerUserId = Guid.Parse(voyagerUserIdClaim.Value);
+        
+        try
+        {
+            // call VoyageService to do the saving of the Voyage and Stops
+            // give down voyageUserId that we got from the token's claims
+            await commentService.DeleteCommentAsync(commentId, voyagerUserId);
+            
+            return Ok("Comment deleted successfully.");
         }
         catch (Exception ex)
         {

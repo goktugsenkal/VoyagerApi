f9b152a3d45c235b5b3381436e01c130a51da53e
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-16 18:12:10 +0300

    add StopDto.cs

---
+++ Core/Dtos/StopDto.cs
@@ -0,0 +1,16 @@
+using Core.Enums;
+
+namespace Core.Dtos;
+
+public class StopDto
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Longitude { get; set; }
+    public string Latitude { get; set; }
+    public int DistanceToNext { get; set; }
+    public int ArrivalTimeToNext { get; set; }
+    public TransportationType TransportationTypeToNextStop { get; set; }
+    
+    public string ImageUrl { get; set; } 
+}

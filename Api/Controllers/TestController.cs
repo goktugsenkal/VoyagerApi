7d35d6fadfeeac3112efda7a4b16d3c26e6448e2
Goktug Mac <goktugsenkal1881@gmail.com>
2025-01-18 16:30:26 +0300

    add TestController: /version endpoint

---
+++ Api/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("version")]
+public class TestController : BaseApiController
+{
+    [HttpGet]
+    public ActionResult<string> Get()
+    {
+        return Ok("0.0.5");
+    }
+}

7a276edf7bebe6ecbb9e85541b75f985166b9bf0
Goktug Mac <goktugsenkal1881@gmail.com>
2025-02-26 00:07:32 +0300

    update VoyagerUserDto to be a clientside-safe version of an entire user

--- Core/Dtos/VoyagerUserDto.cs
+++ Core/Dtos/VoyagerUserDto.cs
@@ -4,6 +4,26 @@ namespace Core.Dtos;
 
 public class VoyagerUserDto
 {
+    public Guid Id { get; set; }
+    
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Bio { get; set; } = string.Empty;
+    
+    public string Email { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public bool EmailConfirmed { get; set; }
+    public bool PhoneNumberConfirmed { get; set; }
+    
+    public int CompletedPlanCount { get; set; }
+    public int PlanCount { get; set; }
+    
+    public int FollowerCount { get; set; }
+    public int FollowingCount { get; set; }
+    public int InspiredCount { get; set; }
+    public int LikeCount { get; set; }
 }

bf36e53a6c813692ae01fe2df82d7f3e715d6f36
Goktug Mac <goktugsenkal1881@gmail.com>
2025-03-01 20:39:16 +0300

    add FeedController and "GET /feed" endpoint

---
+++ Api/Controllers/FeedController.cs
@@ -0,0 +1,23 @@
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[Route("feed")]
+[ApiController]
+public class FeedController(IFeedService feedService) : BaseApiController
+{
+    [HttpGet]
+    [Authorize]
+    public async Task<ActionResult<string>> GetFeed(int pageNumber = 1, int pageSize = 10)
+    {
+        if (pageNumber < 1 || pageSize < 1 || pageSize > 20)
+        {
+            return BadRequest("Page number and page size must be greater than 1 and page size must be less than or equal to 20.");
+        }
+        
+        var voyages = await feedService.GetFeedAsync(pageNumber, pageSize);
+        return Ok(voyages); 
+    }
+}

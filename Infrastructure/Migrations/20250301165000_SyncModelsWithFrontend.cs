49d4605d785d619eed4b1e854bf5db6236132f60
Goktug Mac <goktugsenkal1881@gmail.com>
2025-03-01 20:37:04 +0300

    add migrations "SyncModelsWithFrontend" based on the updated models

---
+++ Infrastructure/Migrations/20250301165000_SyncModelsWithFrontend.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModelsWithFrontend : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Stops");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Users",
+                newName: "ProfilePictureUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BannerPictureUrl",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<List<string>>(
+                name: "ImageUrls",
+                table: "Stops",
+                type: "text[]",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFocalPoint",
+                table: "Stops",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLikedByAuthor",
+                table: "Comments",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BannerPictureUrl",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrls",
+                table: "Stops");
+
+            migrationBuilder.DropColumn(
+                name: "IsFocalPoint",
+                table: "Stops");
+
+            migrationBuilder.DropColumn(
+                name: "IsLikedByAuthor",
+                table: "Comments");
+
+            migrationBuilder.RenameColumn(
+                name: "ProfilePictureUrl",
+                table: "Users",
+                newName: "ImageUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Stops",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}

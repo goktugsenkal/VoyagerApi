524e62db15d0ab86451e2672789d08c2cc49be3d
Goktug Mac <goktugsenkal1881@gmail.com>
2025-03-08 20:29:19 +0300

    add "{userId}/voyages" and "{userId}" endpoints

--- Api/Controllers/UserController.cs
+++ Api/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using Core.Dtos;
 using Core.Interfaces;
+using Core.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,7 +8,7 @@ namespace Api.Controllers;
 
 [Route("users")]
 [ApiController]
-public class UserController(IAuthService authService) : BaseApiController
+public class UserController(IAuthService authService, IUserService userService) : BaseApiController
 {
     [HttpGet("me")]
     [Authorize]
@@ -17,11 +18,44 @@ public class UserController(IAuthService authService) : BaseApiController
         if (voyagerUserId is null)
             return Unauthorized("User ID not found in token claims.");
             
-        var user = await authService.GetUserByIdAsync((Guid)voyagerUserId);
+        var user = await userService.GetUserByIdAsync((Guid)voyagerUserId);
             
         if (user is null)
             return Unauthorized("User not found.");
             
         return Ok(user);
+    }
+    
+    [HttpGet("{userId}")]
+    [Authorize]
+    public async Task<ActionResult<VoyagerUserDto>> GetUserById(string userId)
+    {
+        if (!Guid.TryParse(userId, out var voyagerUserId))
+        {
+            return BadRequest("Invalid user id format.");
+        }
+
+        var user = await userService.GetUserByIdAsync(voyagerUserId);
+
+        if (user is null)
+        {
+            return Unauthorized("User not found.");
+        }
+
+        return Ok(user);
+    }
+
+    
+    [HttpGet("{userId}/voyages")]
+    [Authorize]
+    public async Task<ActionResult<PagedList<VoyageDto>>> GetVoyagesOfAUser
+        (string userId, int pageNumber = 1, int pageSize = 10)
+    {
+        if (pageSize < 1 || pageNumber < 1 || pageSize > 20)
+        {
+            return BadRequest("Page number and page size must be greater than 1 and page size must be less than or equal to 20.");
+        }
+        
+        return await userService.GetVoyagesOfAUserAsync(userId, pageNumber, pageSize);
     }
 }

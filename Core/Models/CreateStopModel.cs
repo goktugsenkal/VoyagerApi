1c74f6e758aec016a7ae9c63935c072f8185ae4e
Goktug Mac <goktugsenkal1881@gmail.com>
2025-03-08 15:14:56 +0300

    update long and lat to be double

--- Core/Models/CreateStopModel.cs
+++ Core/Models/CreateStopModel.cs
@@ -7,8 +7,8 @@ public class CreateStopModel
     public string Name { get; set; }
     public string Description { get; set; }
     
-    public string Longitude { get; set; }
-    public string Latitude { get; set; }
+    public double Longitude { get; set; }
+    public double Latitude { get; set; }
     public int DistanceToNext { get; set; }
     public int ArrivalTimeToNext { get; set; }
     public TransportationType TransportationTypeToNextStop { get; set; }

4cb89a06b81d25f89e784eef9db52f2f022834af
Goktug Mac <goktugsenkal1881@gmail.com>
2025-03-19 03:30:09 +0300

    Add properties to CommentDto for enhanced comment details

--- Core/Dtos/CommentDto.cs
+++ Core/Dtos/CommentDto.cs
@@ -2,5 +2,12 @@ namespace Core.Dtos;
 
 public class CommentDto
 {
+    public Guid Id { get; set; }
     public string Content { get; set; }
+    public bool IsLikedByAuthor { get; set; }
+    public Guid VoyagerUserId { get; set; }
+    public Guid VoyageId { get; set; }
+    public int LikeCount { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }

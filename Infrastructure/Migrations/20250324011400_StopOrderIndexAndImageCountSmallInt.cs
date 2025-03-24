8e34e5e4cc6afb2aace58df3ab6f78969aabafb9
Goktug Mac <goktugsenkal1881@gmail.com>
2025-03-24 13:44:36 +0300

    refactor Stops table to use smallint for OrderIndex and ImageCount columns

---
+++ Infrastructure/Migrations/20250324011400_StopOrderIndexAndImageCountSmallInt.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class StopOrderIndexAndImageCountSmallInt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<short>(
+                name: "OrderIndex",
+                table: "Stops",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<short>(
+                name: "ImageCount",
+                table: "Stops",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "OrderIndex",
+                table: "Stops",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ImageCount",
+                table: "Stops",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "smallint");
+        }
+    }
+}

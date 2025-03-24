9ae2a211ac993f3f5ef0f5e6f23c68df8849d25d
Goktug Mac <goktugsenkal1881@gmail.com>
2025-03-24 13:50:22 +0300

    bump version to 0.2.3

--- Api/Controllers/TestController.cs
+++ Api/Controllers/TestController.cs
@@ -12,6 +12,6 @@ public class TestController(DataContext context) : BaseApiController
     [HttpGet]
     public ActionResult<string> Get()
     {
-        return Ok("0.2.2");
+        return Ok("0.2.3");
     }
 }

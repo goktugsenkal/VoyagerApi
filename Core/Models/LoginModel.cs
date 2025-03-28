baf088aeed36d2ec3c82086947141332e2a16019
Goktug Mac <goktugsenkal1881@gmail.com>
2025-03-28 15:55:05 +0300

    make Username and Password properties required in LoginModel

--- Core/Models/LoginModel.cs
+++ Core/Models/LoginModel.cs
@@ -4,6 +4,6 @@ namespace Core.Models;
 
 public class LoginModel
 {
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public required string Username { get; set; }
+    public required string Password { get; set; }
 }

ea472f22695ef913d5f8f24c78e0de2593467625
Goktug Mac <goktugsenkal1881@gmail.com>
2025-03-28 15:56:27 +0300

    add methods to IUserService for retrieving users by email and updating user details

--- Core/Interfaces/IUserService.cs
+++ Core/Interfaces/IUserService.cs
@@ -1,11 +1,14 @@
 using Core.Dtos;
+using Core.Entities;
 using Core.Models;
 
 namespace Core.Interfaces;
 
 public interface IUserService
 {
-    Task<VoyagerUserDto?> GetUserByIdAsync(Guid id);
+    Task<VoyagerUser?> GetUserByIdAsync(Guid id);
+    Task<VoyagerUser?> GetUserByEmailAsync(string email);
     Task<string?> GetUsernameByIdAsync(Guid id);
     Task<PagedList<VoyageDto>> GetVoyagesOfAUserAsync(string userId,  int pageNumber, int pageSize);
+    Task UpdateUserAsync(VoyagerUser user);
 }

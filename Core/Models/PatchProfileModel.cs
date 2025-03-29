2ac50815b9719571ba5e5ef34e2ba0887f17d20f
Goktug Mac <goktugsenkal1881@gmail.com>
2025-03-29 06:16:10 +0300

    add PatchProfileModel and UpdateProfileModel for profile updates

---
+++ Core/Models/PatchProfileModel.cs
@@ -0,0 +1,10 @@
+namespace Core.Models;
+
+public class PatchProfileModel
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Bio { get; set; }
+    public string? ProfileImageUrl { get; set; }
+    public string? Username { get; set; }
+}
---
+++ Core/Models/UpdateProfileModel.cs
@@ -0,0 +1,9 @@
+namespace Core.Models;
+
+public class UpdateProfileModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Bio { get; set; }
+    public string Username { get; set; }
+}

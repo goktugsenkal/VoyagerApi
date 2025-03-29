3b679109e325eac863197b84342e30bc9cdea1f3
Goktug Mac <goktugsenkal1881@gmail.com>
2025-03-29 06:13:55 +0300

    add UserChangeLog entity to track user modifications

---
+++ Core/Entities/UserChangeLog.cs
@@ -0,0 +1,14 @@
+namespace Core.Entities;
+
+public class UserChangeLog
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string ChangedByUsername { get; set; }
+    public string FieldName { get; set; }
+    public string OldValue { get; set; }
+    public string NewValue { get; set; }
+    public string IPAddress { get; set; }
+    public string UserAgent { get; set; }
+    public DateTime ChangedAt { get; set; }
+}

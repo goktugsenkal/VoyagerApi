dfcbf3ce154dff653cfc5aebc4d4c640aa8c3c30
Goktug Mac <goktugsenkal1881@gmail.com>
2025-04-01 05:06:49 +0300

    add Location property to UpdateProfileModel

--- Core/Models/UpdateProfileModel.cs
+++ Core/Models/UpdateProfileModel.cs
@@ -5,5 +5,6 @@ public class UpdateProfileModel
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Bio { get; set; }
+    public string Location { get; set; }
     public string Username { get; set; }
 }

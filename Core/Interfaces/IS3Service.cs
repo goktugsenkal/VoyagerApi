f6387d6b794d423f0c36c44b4293cee625f5c751
Goktug Mac <goktugsenkal1881@gmail.com>
2025-04-01 05:07:21 +0300

    add ObjectExistsAsync method to IS3Service for checking object existence

--- Core/Interfaces/IS3Service.cs
+++ Core/Interfaces/IS3Service.cs
@@ -2,6 +2,7 @@ namespace Core.Interfaces;
 
 public interface IS3Service
 {
+    Task<bool> ObjectExistsAsync(string key);
     string GeneratePreSignedUploadUrl(string objectKey, TimeSpan expiration);
     string GeneratePreSignedDownloadUrl(string objectKey, TimeSpan expiration);
 }

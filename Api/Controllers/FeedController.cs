963784af24eff0c3a2b4fbd496ddf8634bc320bf
Goktug Mac <goktugsenkal1881@gmail.com>
2025-04-11 14:35:51 +0300

    feat: enhance GetFeed method to include user ID retrieval for improved data access

--- Api/Controllers/FeedController.cs
+++ Api/Controllers/FeedController.cs
@@ -14,12 +14,19 @@ public class FeedController(IFeedService feedService) : BaseApiController
     [Authorize]
     public async Task<ActionResult<PagedList<VoyageDto>>> GetFeed(int pageNumber = 1, int pageSize = 10)
     {
+        // return 401 if User's ID can't be found
+        var voyagerUserId = GetUserIdFromTokenClaims();
+        if (voyagerUserId == null)
+        {
+            return Unauthorized("User ID not found in token claims.");
+        }
+        
         if (pageNumber < 1 || pageSize < 1 || pageSize > 20)
         {
             return BadRequest("Page number and page size must be greater than 1 and page size must be less than or equal to 20.");
         }
         
-        var voyages = await feedService.GetFeedAsync(pageNumber, pageSize);
+        var voyages = await feedService.GetFeedAsync(pageNumber, pageSize, voyagerUserId.Value); // value is checked for null, safe
         return Ok(voyages); 
     }
 }

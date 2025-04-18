df0235a5e9ed4c348c1843696aaf2139d316763f
Goktug Mac <goktugsenkal1881@gmail.com>
2025-04-18 15:25:07 +0300

    refactor: replace ImageUrls with MediaKeys and remove ImageCount in migration files

---
+++ Infrastructure/Migrations/20250417191343_AddMediaTypeSupport.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMediaTypeSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageCount",
+                table: "Stops");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrls",
+                table: "Voyages",
+                newName: "MediaKeys");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrls",
+                table: "Stops",
+                newName: "MediaKeys");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MediaKeys",
+                table: "Voyages",
+                newName: "ImageUrls");
+
+            migrationBuilder.RenameColumn(
+                name: "MediaKeys",
+                table: "Stops",
+                newName: "ImageUrls");
+
+            migrationBuilder.AddColumn<short>(
+                name: "ImageCount",
+                table: "Stops",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+        }
+    }
+}

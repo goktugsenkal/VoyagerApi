fcc2135e4feddcfda915b4bb072baf77c52629b1
Goktug Mac <goktugsenkal1881@gmail.com>
2025-04-27 06:40:46 +0300

    fix: update API version to 0.4.004 in VersionInfo

--- Core/Constants/VersionInfo.cs
+++ Core/Constants/VersionInfo.cs
@@ -2,5 +2,5 @@ namespace Core.Constants;
 
 public static class VersionInfo
 {
-    public const string ApiVersion = "Production: 0.4.000";
+    public const string ApiVersion = "Production: 0.4.004";
 }

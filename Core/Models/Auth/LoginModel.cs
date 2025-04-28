a6120fe9a56f455e80733d095dfae13fc0cd3ea8
Goktug Mac <goktugsenkal1881@gmail.com>
2025-04-28 22:18:20 +0300

    feat: add DeviceId property to LoginModel for enhanced device tracking

--- Core/Models/Auth/LoginModel.cs
+++ Core/Models/Auth/LoginModel.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel;
+
 namespace Core.Models;
 
 public class LoginModel
 {
     public required string Username { get; set; }
     public required string Password { get; set; }
+    public required Guid DeviceId { get; set; }
 }

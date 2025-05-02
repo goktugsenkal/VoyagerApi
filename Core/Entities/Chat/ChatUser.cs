c13af4d8b93fab58ac3094e68eaeb3d22cec4f38
Goktug Mac <goktugsenkal1881@gmail.com>
2025-05-02 05:51:22 +0300

    add ChatUser class to represent users in chat with properties for display name, DM permissions, status message, last seen time, and user reference

---
+++ Core/Entities/Chat/ChatUser.cs
@@ -0,0 +1,12 @@
+namespace Core.Entities.Chat;
+
+public class ChatUser : UpdatableBaseEntity
+{
+    public string? DisplayName { get; set; }
+    public bool AllowDMs { get; set; }
+    public string? StatusMessage { get; set; }
+    public DateTime LastSeen { get; set; }
+    public bool ShowLastSeen { get; set; }
+    
+    public VoyagerUser User { get; set; }
+}

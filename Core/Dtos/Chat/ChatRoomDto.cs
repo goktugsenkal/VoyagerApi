3332685cbc26ad539220c2a8634eb65b765d9e44
Goktug Mac <goktugsenkal1881@gmail.com>
2025-05-05 00:28:22 +0300

    feat: add IsGroup property and initialize ParticipantIds in ChatRoomDto

--- Core/Dtos/Chat/ChatRoomDto.cs
+++ Core/Dtos/Chat/ChatRoomDto.cs
@@ -6,6 +6,10 @@ public class ChatRoomDto
     public DateTime CreatedAt { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public string Type { get; set; }
+    public bool IsGroup { get; set; }
     public MessageDto? LastMessage { get; set; }
+    public List<Guid> ParticipantIds { get; set; } = [];
+    
+    public string ImageUrl { get; set; } = "default/image.jpg";
+    public string? BannerUrl { get; set; }
 }

f217bc2a4c687eb12b711ccb19b7ca864db8c211
Goktug Mac <goktugsenkal1881@gmail.com>
2025-05-06 01:06:06 +0300

    fix: correct ApiVersion string by adding missing build number

--- Core/Constants/VersionInfo.cs
+++ Core/Constants/VersionInfo.cs
@@ -2,5 +2,5 @@ namespace Core.Constants;
 
 public static class VersionInfo
 {
-    public const string ApiVersion = "Staging: 0.4.140 31";
+    public const string ApiVersion = "Staging: 0.4.140 31 31";
 }

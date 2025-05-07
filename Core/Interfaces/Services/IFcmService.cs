5f24fbdaa0174c4589071fb1b34c424a5a956dd6
Goktug Mac <goktugsenkal1881@gmail.com>
2025-05-07 23:56:28 +0300

    Implement FCM notification service with SendNotificationAsync method

--- Core/Interfaces/Services/IFcmService.cs
+++ Core/Interfaces/Services/IFcmService.cs
@@ -2,5 +2,5 @@ namespace Core.Interfaces.Services;
 
 public interface IFcmService
 {
-    
+    Task<string> SendNotificationAsync(string fcmToken, string title, string body, IDictionary<string,string>? data = null);
 }

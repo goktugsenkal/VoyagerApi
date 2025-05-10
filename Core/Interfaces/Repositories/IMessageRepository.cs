fb328c726092f1bac8c70a18077df6e08a44222a
Goktug Mac <goktugsenkal1881@gmail.com>
2025-05-10 22:17:57 +0300

    Add GetReadReceiptAsync method to IMessageRepository for retrieving read receipts by message and user ID, enhancing message tracking capabilities.

--- Core/Interfaces/Repositories/IMessageRepository.cs
+++ Core/Interfaces/Repositories/IMessageRepository.cs
@@ -7,7 +7,7 @@ public interface IMessageRepository
     Task AddAsync(Message message);
     Task RemoveAsync(Message message);
     Task<Message?> GetByIdAsync(Guid messageId);
-    
+    Task<ChatMessageReadReceipt?> GetReadReceiptAsync(Guid messageId, Guid userId);
     Task AddReadReceiptAsync(ChatMessageReadReceipt receipt);
     Task<ChatMessageDeliveredReceipt?> GetDeliveredReceiptAsync(Guid messageId, Guid userId);
     Task AddDeliveredReceiptAsync(ChatMessageDeliveredReceipt receipt);
